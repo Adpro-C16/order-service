c02d7d22a9b39f55fdb23447b70b43344b171e83
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-23 22:06:24 +0700

    feat: Adding migration files

---
+++ migrations/20240423070318_order.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS "orders" (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    quantity INTEGER DEFAULT 1,
+    subtotal INTEGER NOT NULL,
+    status VARCHAR(255) NOT NULL DEFAULT 'Waiting Payment',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
