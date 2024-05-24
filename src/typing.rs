9bb3318d204a3dcbc052d9fc0a69048c77ebb954
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-24 23:52:57 +0700

    feat: Adding types

---
+++ src/typing.rs
@@ -0,0 +1,24 @@
+use rocket::http::Status;
+use rocket::response::status::Custom;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
+#[derive(Serialize, Debug, Clone, PartialEq)]
+#[serde(crate = "rocket::serde")]
+pub struct ErrorResponse {
+    pub status_code: Status,
+    pub message: &'static str,
+}
+
+pub fn error_response(status_code: Status, message: &'static str) -> Custom<Json<ErrorResponse>> {
+    return Custom(
+        status_code,
+        Json::from(ErrorResponse {
+            status_code: status_code,
+            message: message,
+        }),
+    );
+}
+
+pub type Error = Custom<Json<ErrorResponse>>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
