85ed4b114a038c3d0f129c68c7439500f71e6719
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-26 00:56:11 +0700

    feat: Adding CORS

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::env;
 
 use rocket::{get, routes};
 
+use autometrics::prometheus_exporter;
 use dotenvy::dotenv;
-use routes::order::{create_order, get_orders};
+use rocket_cors::AllowedOrigins;
+use routes::order::{create_order, get_order, get_orders, pay_order};
 use sqlx::postgres::PgPoolOptions;
+
 pub mod guard;
 pub mod model;
 pub mod repository;
@@ -16,9 +19,16 @@ fn index() -> &'static str {
     "Heymart C14 Payment Service"
 }
 
+#[get("/metrics")]
+pub fn metrics() -> String {
+    prometheus_exporter::encode_to_string().unwrap()
+}
+
 #[rocket::main]
 async fn main() {
     dotenv().ok();
+
+    prometheus_exporter::init();
 
     let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
 
@@ -28,10 +38,23 @@ async fn main() {
         .await
         .unwrap();
 
+    let allowed_origins = AllowedOrigins::all();
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
     rocket::build()
         .manage(pool)
-        .mount("/", routes![index])
-        .mount("/orders", routes![create_order, get_orders])
+        .attach(cors)
+        .mount("/", routes![index, metrics])
+        .mount(
+            "/orders",
+            routes![create_order, get_orders, pay_order, get_order],
+        )
         .launch()
         .await
         .unwrap();
